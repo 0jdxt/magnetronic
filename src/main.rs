fa4cbf405d3ebbc760a6cffb7e8ae3b29e03c556
Jay <jytrn@protonmail.com>
2025-06-07 19:11:36 +0100

    implement lists

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use serde_json;
+use serde_json::Value;
 use std::env;
 
 // Available if you need it!
@@ -15,13 +15,35 @@ fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
         let number_string = &encoded_value[..colon_index];
         let number: usize = number_string.parse().unwrap();
         let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        return serde_json::Value::String(string.to_string());
+        return Value::String(string.to_string());
     } else if type_char == 'i' {
         // Example: "i52e" -> 52
         let end = encoded_value.find('e').unwrap();
         let number_string = &encoded_value[1..end];
         let number = number_string.parse().unwrap();
-        return serde_json::Value::Number(number);
+        return Value::Number(number);
+    } else if type_char == 'l' {
+        let mut values = Vec::new();
+        let mut i = 1;
+        while encoded_value.chars().nth(i) != Some('e') {
+            let v = decode_bencoded_value(&encoded_value[i..]);
+            // println!("processing: {}", &encoded_value[i..]);
+
+            let skip = match &v {
+                Value::String(s) => s.len() + 2,
+                Value::Number(n) => {
+                    let n = n.as_i64().unwrap();
+                    n.abs().ilog10() as usize + 3 + n.is_negative() as usize
+                }
+                _ => panic!("unknown value"),
+            };
+
+            // println!("remaining: {}", &encoded_value[i + skip..]);
+            // println!("{} - skip {}", v, skip);
+            values.push(v);
+            i += skip;
+        }
+        return serde_json::Value::Array(values);
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
