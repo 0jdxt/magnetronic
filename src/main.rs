b70c8ede2ec03b81245eb793972d2e14e4f892db
Jay <jytrn@protonmail.com>
2025-06-07 18:46:50 +0100

    parse integers

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,21 @@ use std::env;
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
     // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_digit(10) {
+    let type_char = encoded_value.chars().next().unwrap();
+
+    if type_char.is_digit(10) {
         // Example: "5:hello" -> "hello"
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<i64>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number as usize];
+        let number: usize = number_string.parse().unwrap();
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
         return serde_json::Value::String(string.to_string());
+    } else if type_char == 'i' {
+        // Example: "i52e" -> 52
+        let end = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..end];
+        let number = number_string.parse().unwrap();
+        return serde_json::Value::Number(number);
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
